472d3041ac63d85f1bbd6a99f16362f74a9f3ecc
Vivek Thapar <vivekthapar3@gmail.com>
2023-10-17 00:46:31 +1000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 3000;
+//const port = process.env.PORT || 3000;
+const port = 3000;
 
 // Middleware
 app.use(bodyParser.json());
